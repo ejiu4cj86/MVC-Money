ea4493331a3f6771d6aa84bb95a8da4efa8980db
RogerKuo <ejiu4cj86@gmail.com>
2019-07-12 15:22:36 +0800

    Add list.

--- MVC-Money/Controllers/MoneyController.cs
+++ MVC-Money/Controllers/MoneyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Money.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,5 +19,28 @@ namespace MVC_Money.Controllers
         {
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult List()
+        {
+            Random random = new Random();
+            var diffDay = 0;
+            var list = new List<MoneyViewModel>();
+            string[] categoryArray = { "存入", "支出" };
+            for (int i = 0; i < 100; i++)
+            {
+                diffDay -= random.Next(0, 7);
+                var date = DateTime.Now.AddDays(diffDay);
+                list.Add(new MoneyViewModel
+                {
+                    Category = categoryArray[random.Next(2)],
+                    Date = date,
+                    Money = (int)random.Next(10000)
+                });
+            }
+            return View(list);
+        }
+
+
     }
 }
--- MVC-Money/Models/ViewModels/MoneyViewModel.cs
+++ MVC-Money/Models/ViewModels/MoneyViewModel.cs
@@ -10,10 +10,15 @@ namespace MVC_Money.Models.ViewModels
     {
         [Display(Name = "類別")]
         public String Category { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         [Display(Name = "日期")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         [Display(Name = "金額")]
         public int Money { get; set; }
+
         [Display(Name = "備註")]
         public string Note { get; set; }
     }
