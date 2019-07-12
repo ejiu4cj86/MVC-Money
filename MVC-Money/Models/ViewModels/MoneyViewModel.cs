7a2f4a93ee0627ac0494186a66c21299c69b41d2
RogerKuo <ejiu4cj86@gmail.com>
2019-07-12 10:59:56 +0800

    Add view.

---
+++ MVC-Money/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Money.Models.ViewModels
+{
+    public class MoneyViewModel
+    {
+        [Display(Name = "類別")]
+        public String Category { get; set; }
+        [Display(Name = "日期")]
+        public DateTime Date { get; set; }
+        [Display(Name = "金額")]
+        public int Money { get; set; }
+        [Display(Name = "備註")]
+        public string Note { get; set; }
+    }
+}
